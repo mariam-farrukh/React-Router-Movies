70dc524ea289ddf1cc885d380ce602b67522694d
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-07-22 17:03:55 -0500

    Fixed error with project

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState(null);
+  //need to put null here because {} would cause an error in code.
+  // Null explicitly states that it is empty while an empty useState may not be clear.
+  console.log(props)
  
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -13,6 +16,7 @@ const Movie = (props) => {
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
+          console.log(response);
         })
         .catch(error => {
           console.error(error);
@@ -29,6 +33,7 @@ const Movie = (props) => {
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
+  console.log(movie);
 
   const { title, director, metascore, stars } = movie;
   return (
