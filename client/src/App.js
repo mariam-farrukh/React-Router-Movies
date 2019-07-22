5856cbee9ae0416fb5065a6ced66f5f9c05cce8f
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-07-22 15:03:59 -0500

    Added imports, added routes, but now looking to debug

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 
 import SavedList from './Movies/SavedList';
 
+import { Route } from "react-router-dom";
+import Movie from "./Movies/Movie.js";
+import MovieList from "./Movies/MovieList.js";
+
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
@@ -12,7 +16,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route path="/" exact component={MovieList}/>
+      <Route path="/movies/" component={Movie}/>
     </div>
   );
 };
